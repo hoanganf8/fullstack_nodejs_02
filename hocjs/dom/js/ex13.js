7ed38ef06aef28cca2253a45e58cf7590c241ec5
Hoàng An F8 <hoangan@fullstack.edu.vn>
2023-09-17 07:55:36 +0700

    Buoi 28

--- hocjs/dom/js/ex13.js
+++ hocjs/dom/js/ex13.js
@@ -23,6 +23,9 @@ progressBar.addEventListener("mousedown", function (e) {
     initialClientX = e.clientX;
     isDrag = true;
     handleUpdateValue(value);
+
+    var currentTime = (value / 100) * audio.duration;
+    audio.currentTime = currentTime;
   }
 });
 
@@ -104,3 +107,127 @@ audio.addEventListener("timeupdate", function () {
 
   handleUpdateValue(value);
 });
+
+//Xây dựng tính năng karaoke
+
+var karaoke = document.querySelector(".karaoke");
+var karaokeInner = karaoke.querySelector(".karaoke-inner");
+var karaokeContent = karaoke.querySelector(".karaoke-content");
+var closeBtn = karaoke.querySelector(".close");
+var karaokePlay = karaoke.querySelector(".play");
+var playerWrap = document.querySelector(".player-wrap");
+
+karaokePlay.addEventListener("click", function () {
+  karaoke.classList.add("show");
+  playerWrap.classList.add("bottom");
+});
+
+closeBtn.addEventListener("click", function () {
+  karaoke.classList.remove("show");
+  playerWrap.classList.remove("bottom");
+});
+console.log(lyric);
+
+var currentScreen;
+
+//Lắng nghe sự kiện timeupdate của audio
+audio.addEventListener("timeupdate", function () {
+  var currentTime = Math.floor(audio.currentTime * 1000);
+
+  var index = lyric.findIndex(function (sentence) {
+    sentence = sentence.words;
+    return (
+      currentTime >= sentence[0].startTime &&
+      currentTime <= sentence[sentence.length - 1].endTime
+    );
+  });
+
+  //Hiển thị 2 câu đầu tiên nếu lời trước 5 giây
+  var firstTime = lyric[0].words[0].startTime;
+  var firstRange = currentTime - firstTime;
+  if (firstRange > -5000 && firstRange < 0) {
+    index = 0;
+  }
+
+  if (index !== -1) {
+    handleColor(currentTime);
+
+    var screen = Math.floor(index / 2 + 1);
+
+    if (screen !== currentScreen) {
+      var offset = (screen - 1) * 2;
+
+      if (index >= offset && index < offset + 2) {
+        var pTag = "";
+        for (var i = offset; i < offset + 2; i++) {
+          var sentence = lyric[i].words
+            .map(function (item) {
+              return item.data;
+            })
+            .join(" ");
+          pTag += `<p 
+          data-start-time="${lyric[i].words[0].startTime}"
+          data-end-time="${lyric[i].words[lyric[i].words.length - 1].endTime}"
+          data-words=${JSON.stringify(lyric[i].words)}
+          >${sentence}<span>${sentence}</span>
+          </p>`;
+        }
+
+        karaokeContent.innerHTML = pTag;
+      }
+
+      currentScreen = screen;
+    }
+  }
+});
+
+var handleColor = function (currentTime) {
+  var sentencesEl = karaokeContent.children;
+  Array.from(sentencesEl).forEach(function (sentence) {
+    var startTime = sentence.dataset.startTime;
+    var endTime = sentence.dataset.endTime;
+
+    if (startTime && endTime && currentTime >= startTime) {
+      //So sánh current với startTime của từng từ -> Biết được đang hát tới từ nào trong 1 câu
+      var words = JSON.parse(sentence.dataset.words);
+
+      var result = words[0];
+
+      words.forEach(function (item, index) {
+        if (index === 0) {
+          result = words[0];
+        } else if (currentTime > item.endTime && index > 0) {
+          result = words[index];
+        }
+      });
+
+      var endTimeResult = result.endTime;
+
+      //Tính tổng thời gian của cả câu
+      var sentenceTime = endTime - startTime;
+
+      //Tính thời gian của từ với thời gian bắt đầu của câu
+      var time = endTimeResult - startTime;
+
+      var rate = (time * 100) / sentenceTime;
+
+      sentence.children[0].style.width = `${rate}%`;
+      sentence.children[0].style.transition = `width ${
+        result.endTime - result.startTime
+      }ms linear`;
+    }
+  });
+};
+
+/*
+screen 1: index = 0, index = 1
+screen 2: index = 2, index = 3
+screen 3: index = 4, index = 5
+
+Công thức: screen = index / 2 + 1
+
+index = 1 -> Hiển thị index = 2 thay thế cho 
+index=0
+
+index = 2 -> Hiển thị index = 3 thay thế cho index=1
+*/
