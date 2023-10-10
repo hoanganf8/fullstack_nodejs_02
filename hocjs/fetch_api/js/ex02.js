ca42d02c4e7a54e78a2f58b326f7cb1cb5410462
Hoàng An F8 <hoangan@fullstack.edu.vn>
2023-10-10 20:03:06 +0700

    Update code

--- hocjs/fetch_api/js/ex02.js
+++ hocjs/fetch_api/js/ex02.js
@@ -1,20 +1,48 @@
 import { client } from "./client.js";
 
-const getPosts = async () => {
-  const { data: posts } = await client.get(`/posts`);
-  const { data: users } = await client.get("/users");
-  console.log(posts);
-  console.log(users);
+const render = (posts) => {
+  const stripHtmlTag = (html) => html.replace(/<[^>]*>?/gm, "");
+  const postsEl = document.querySelector(".posts");
+  postsEl.innerHTML = `
+  <div class="row g-3">
+    ${posts
+      .map(
+        ({ title, excerpt }) => `<div class="col-6 col-lg-4">
+    <div class="border p-3">
+      <h2><a href="#" class="text-decoration-none">${stripHtmlTag(
+        title,
+      )}</a></h2>
+      <p>
+        ${stripHtmlTag(excerpt)}
+      </p>
+    </div>
+  </div>`,
+      )
+      .join("")}
+  </div>`;
 };
 
-// getPosts();
+const getPosts = async (query = {}) => {
+  //Chuyển object query => query string
+  const queryString = new URLSearchParams(query).toString();
+  const { data: posts } = await client.get(`/posts?${queryString}`);
+  render(posts);
+};
 
-const postUser = async () => {
-  const { response } = await client.post("/users", {
-    name: "Hoàng An",
-    email: "hoangan.web@gmail.com",
+getPosts({
+  _sort: "id",
+  _order: "desc",
+});
+
+const searchForm = document.querySelector(".search");
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const keywordEl = e.target.querySelector(".keyword");
+  const keyword = keywordEl.value;
+
+  getPosts({
+    q: keyword,
   });
-  console.log(response);
-};
 
-// postUser();
+  keywordEl.value = "";
+});
