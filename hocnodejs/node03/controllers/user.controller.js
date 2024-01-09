2ff1efe32a9e9c67316c2a122520188fcd4af183
Hoàng An F8 <hoangan@fullstack.edu.vn>
2024-01-09 21:01:37 +0700

    Update code

--- hocnodejs/node03/controllers/user.controller.js
+++ hocnodejs/node03/controllers/user.controller.js
@@ -35,4 +35,44 @@ module.exports = {
     });
     res.render("users/index", { users, moment });
   },
+  add: (req, res) => {
+    res.render("users/add");
+  },
+  handleAdd: async (req, res) => {
+    const body = req.body;
+    body.status = +body.status === 1;
+    console.log(body);
+    const user = await User.create(body);
+    return res.redirect("/users");
+  },
+  edit: async (req, res, next) => {
+    const { id } = req.params;
+    // const user = await User.findByPk(id);
+    try {
+      const user = await User.findOne({ where: { id: id } });
+      if (!user) {
+        throw new Error("Người dùng không tồn tại");
+      }
+      res.render("users/edit", { user });
+    } catch (e) {
+      return next(e);
+    }
+  },
+  handleEdit: async (req, res) => {
+    const { id } = req.params;
+    const body = req.body;
+    body.status = +body.status === 1;
+    const status = await User.update(body, {
+      where: { id },
+    });
+
+    return res.redirect(`/users/edit/${id}`);
+  },
+  delete: async (req, res) => {
+    const { id } = req.params;
+    const status = await User.destroy({
+      where: { id },
+    });
+    return res.redirect(`/users`);
+  },
 };
